37bfbb97c5905a6d3696091728fdc938a2eea37a
SilMoolhuijsen <74902979+SilMoolhuijsen@users.noreply.github.com>
2021-10-13 12:20:20 +0000

    speler block blijft in speelveld

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var gameState = 0;
 
+var punten1 = 0;
+
 function draw() {
   background(225);
   if (gameState == 1) {
@@ -21,9 +23,8 @@ function setup() {
   block_bovenR = new Block(325, 0, 175, 10);
   block_speler = new Spelerblocks(210, 350, 80, 10, 0)
   block_speler_2 = new Spelerblocks(210, 50, 80, 10, 0)
-  puck1 = new Puck(300, 300, 50, 50, 5, 5, "black")
+  puck1 = new Puck(250, 200, 50, 50, 5, 5, "black")
 }
-
 
 class Puck {
   constructor(x, y, w, h, vx, vy, c) {
@@ -80,9 +81,6 @@ class Spelerblocks {
   }
 }
 
-
-//var [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
-
 function game() {
   background(225);
   block_links.drawBlock();
@@ -96,10 +94,13 @@ function game() {
   puck1.drawPuck();
   fill(0);
 
-  if (block_links.x + block_links.w > block_speler.x && block_speler.x + block_speler.w > block_rechts.x){
-    block.speler.vx = 0;
-  }
+  textSize(40);
+  text(punten1, 240, 100)
+  fill(10);
 
+  if (block_links.x + block_links.w > block_speler.x || block_speler.x + block_speler.w > block_rechts.x){
+    block_speler.xspeed = block_speler.xspeed * -1;
+  }
   if (puck1.y + 0.58 * puck1.h > block_speler.y && puck1.y - 0.58 * puck1.h < block_speler.y + block_speler.h && puck1.x + 0.58 * puck1.w > block_speler.x && puck1.x - 0.58 * puck1.w < block_speler.x + block_speler.w){
     puck1.vy = puck1.vy * -1;
   }
@@ -118,18 +119,18 @@ function game() {
   if (puck1.x + 0.58 * puck1.w > block_rechts.x){
     puck1.vx = puck1.vx * -1;
   }
-  if (puck1.y + 0.58 * puck1.h > block_benedenL.y && puck1.x - puck1.w * 0.55 < block_benedenL.x + block_benedenL.w){
+  if (puck1.y + 0.58 * puck1.h > block_benedenL.y && puck1.x - puck1.w * 0.50 < block_benedenL.x + block_benedenL.w){
     puck1.vy = puck1.vy * -1;
   }
-  if (puck1.y + 0.58 * puck1.h > block_benedenR.y && puck1.x + puck1.w * 0.55 > block_benedenR.x){
+  if (puck1.y + 0.58 * puck1.h > block_benedenR.y && puck1.x + puck1.w * 0.50 > block_benedenR.x){
     puck1.vy = puck1.vy * -1;
   }
-  if (puck1.y - 0.58 * puck1.h < block_bovenL.y + block_bovenL.h && puck1.x - puck1.w * 0.55 < block_bovenL.x + block_bovenL.w){
+  if (puck1.y - 0.58 * puck1.h < block_bovenL.y + block_bovenL.h && puck1.x - puck1.w * 0.50 < block_bovenL.x + block_bovenL.w){
     puck1.vy = puck1.vy * -1;
   }
-  if (puck1.y - 0.58 * puck1.h < block_bovenR.y + block_bovenR.h && puck1.x + puck1.w * 0.55 > block_bovenR.x){
+  if (puck1.y - 0.58 * puck1.h < block_bovenR.y + block_bovenR.h && puck1.x + puck1.w * 0.50 > block_bovenR.x){
     puck1.vy = puck1.vy * -1;
-  }
+  } 
 
  if (mouseX > 250){
    block_speler_2.xspeed = 4;
@@ -140,10 +141,11 @@ function game() {
 
  if (puck1.y > 370){
    if (puck1.x > 175 && puck1.x < 325){
-     puck1.x = 300
-     puck1.y = 300
+     puck1.x = 250
+     puck1.y = 200
      puck1.vx = 5
      puck1.vy = 5
+     punten1 = punten1 + 1
    }
  }
 
