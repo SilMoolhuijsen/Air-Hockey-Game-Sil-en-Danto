42e48bfbd4103d8cbe8b095ad082ff0eb8f22f58
Danto Rosier <74553165+110671Danto@users.noreply.github.com>
2021-10-13 12:50:46 +0000

    classes verbetert

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ var gameState = 0;
 
 var punten1 = 0;
 
+var punten2 = 0;
+
+
+var spelers = [];
+
 function draw() {
   background(225);
   if (gameState == 1) {
@@ -15,14 +20,13 @@ function draw() {
 
 function setup() {
   createCanvas(500, 400);
-  block_links = new Block(0, 0, 10, 400);
-  block_rechts = new Block(489, 0, 10, 400);
-  block_benedenL = new Block(0, 390, 175, 10);
-  block_benedenR = new Block(325, 390, 175, 10);
-  block_bovenL = new Block(0, 0, 175, 10);
-  block_bovenR = new Block(325, 0, 175, 10);
+  goal = new Block(250, 0, 200, 40);
   block_speler = new Spelerblocks(210, 350, 80, 10, 0)
-  block_speler_2 = new Spelerblocks(210, 50, 80, 10, 0)
+  block_speler_2 = new Computerblocks(210, 50, 80, 10, 0)
+
+  spelers.push(goal);
+  spelers.push(block_speler);
+  spelers.push(block_speler_2);
   puck1 = new Puck(250, 200, 50, 50, 5, 5, "black")
 }
 
@@ -39,14 +43,14 @@ class Puck {
 
   drawPuck() {
     fill(this.c);
-    ellipse(this.x, this.y, this.w, this.h);
+    rect(this.x, this.y, this.w, this.h);
     this.x = this.x + this.vx;
     this.y = this.y + this.vy;
 
-    if (this.x <= 20 || this.x >= 480) {
+    if (this.x <= 10 || this.x + this.w >= width - 10) {
       this.vx = this.vx * -1;
     }
-    if (this.y <= 24 || this.y >= 380) {
+    if (this.y <= 10 || this.y + this.h >= height - 10) {
       this.vy = this.vy * -1;
     }
   }
@@ -58,10 +62,19 @@ class Block {
     this.y = y;
     this.w = w;
     this.h = h;
+    this.c = "red";
   }
-  drawBlock() {
+  draw() {
+    fill(this.c);
     rect(this.x, this.y, this.w, this.h);
-    
+  }
+
+  checkCollision() {
+    if (puck1.y + puck1.h > this.y && puck1.y < this.y + this.h) {
+      if (puck1.x + puck1.w > this.x && puck1.x < this.x + this.w) {
+        console.log("SCORE!")
+      }
+    }
   }
 }
 
@@ -72,25 +85,59 @@ class Spelerblocks {
     this.w = w;
     this.h = h;
     this.xspeed = xspeed;
-    
+
   }
-  drawSpelerblocks() {
+  draw() {
+    this.x = mouseX;
     rect(this.x, this.y, this.w, this.h);
     this.x = this.x + this.xspeed;
-    
+  }
+
+  checkCollision() {
+    if (puck1.y + puck1.h > this.y) {
+      if (puck1.x + puck1.w > this.x && puck1.x < this.x + this.w) {
+        puck1.vy = -puck1.vy;
+      }
+    }
   }
 }
 
+class Computerblocks {
+  constructor(x, y, w, h, xspeed) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.xspeed = xspeed;
+
+  }
+  draw() {
+    if (keyIsDown(LEFT_ARROW)) {
+      this.x -= 5;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.x += 5;
+    }
+    rect(this.x, this.y, this.w, this.h);
+  }
+
+  checkCollision() {
+    if (puck1.y + puck1.h > this.y && puck1.y < this.y + this.h) {
+      if (puck1.x + puck1.w > this.x && puck1.x < this.x + this.w) {
+        puck1.vy = -puck1.vy;
+      }
+    }
+  }
+}
+
+
 function game() {
   background(225);
-  block_links.drawBlock();
-  block_rechts.drawBlock();
-  block_benedenL.drawBlock();
-  block_benedenR.drawBlock();
-  block_bovenL.drawBlock();
-  block_bovenR.drawBlock();
-  block_speler.drawSpelerblocks();
-  block_speler_2.drawSpelerblocks();
+
+  spelers.forEach((s) => {
+    s.draw();
+    s.checkCollision();
+  })
   puck1.drawPuck();
   fill(0);
 
@@ -98,119 +145,31 @@ function game() {
   text(punten1, 240, 100)
   fill(10);
 
-  if (block_links.x + block_links.w > block_speler.x || block_speler.x + block_speler.w > block_rechts.x){
-    block_speler.xspeed = block_speler.xspeed * -1;
-  }
-  if (puck1.y + 0.58 * puck1.h > block_speler.y && puck1.y - 0.58 * puck1.h < block_speler.y + block_speler.h && puck1.x + 0.58 * puck1.w > block_speler.x && puck1.x - 0.58 * puck1.w < block_speler.x + block_speler.w){
-    puck1.vy = puck1.vy * -1;
-  }
+  text(punten2, 240, 300)
 
-  if (puck1.y + 0.58 * puck1.h < block_speler_2.y && puck1.y > block_speler_2.y + block_speler_2.h && puck1.x + 0.58 * puck1.w > block_speler_2.x && puck1.x < block_speler_2.x + block_speler_2.w){
-    puck1.vy = puck1.vy * -1;
+  if (puck1.y > 370) {
+    if (puck1.x > 175 && puck1.x < 325) {
+      puck1.x = 250
+      puck1.y = 200
+      puck1.vx = 5
+      puck1.vy = 5
+      punten1 = punten1 + 1
+    }
   }
-
-  else{
-    puck1.c = "black";
-  }
-
-  if (puck1.x - 0.58 * puck1.w < block_links.x + block_links.w){
-    puck1.vx = puck1.vx * -1;
-  }
-  if (puck1.x + 0.58 * puck1.w > block_rechts.x){
-    puck1.vx = puck1.vx * -1;
-  }
-  if (puck1.y + 0.58 * puck1.h > block_benedenL.y && puck1.x - puck1.w * 0.50 < block_benedenL.x + block_benedenL.w){
-    puck1.vy = puck1.vy * -1;
-  }
-  if (puck1.y + 0.58 * puck1.h > block_benedenR.y && puck1.x + puck1.w * 0.50 > block_benedenR.x){
-    puck1.vy = puck1.vy * -1;
-  }
-  if (puck1.y - 0.58 * puck1.h < block_bovenL.y + block_bovenL.h && puck1.x - puck1.w * 0.50 < block_bovenL.x + block_bovenL.w){
-    puck1.vy = puck1.vy * -1;
-  }
-  if (puck1.y - 0.58 * puck1.h < block_bovenR.y + block_bovenR.h && puck1.x + puck1.w * 0.50 > block_bovenR.x){
-    puck1.vy = puck1.vy * -1;
-  } 
-
- if (mouseX > 250){
-   block_speler_2.xspeed = 4;
- }
- if (mouseX < 250){
-   block_speler_2.xspeed = -4;
- }
-
- if (puck1.y > 370){
-   if (puck1.x > 175 && puck1.x < 325){
-     puck1.x = 250
-     puck1.y = 200
-     puck1.vx = 5
-     puck1.vy = 5
-     punten1 = punten1 + 1
-   }
- }
 
 }
 
-// 	fill(0);
-// 	text("Use the arrow keys (or WASD) to move the square around", 25, 25);
-
-// 	fill(0, 255, 0);
-// 	rect(xpos, ypos, 50, 50);
-
-// 	if(xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
-// 	if(ypos >= 0 && ypos + 50 <= 500) ypos += yspeed;
-// }
 function menu() {
   background("#aaaaaa");
   text("press 1 to start", 70, 70);
   text("press 2 to open menu", 70, 100);
 }
 
-
 function keyPressed() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			block_speler.xspeed = -4;
-			break;
-		case 39:
-		case 68:
-			block_speler.xspeed = 4;
-			break;
-		case 38:
-		case 87:
-			yspeed = -4;
-			break;
-		case 40:
-		case 83:
-			yspeed = 4;
-			break;
-	}
   if (keyCode == 49) {
     gameState = 1;
   }
   if (keyCode == 50) {
     gameState = 0;
   }
-}
-
-function keyReleased() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			block_speler.xspeed = 0;
-			break;
-		case 39:
-		case 68:
-			block_speler.xspeed = 0;
-			break;
-		case 38:
-		case 87:
-			yspeed = 0;
-			break;
-		case 40:
-		case 83:
-			yspeed = 0;
-			break;
-	}
 }
